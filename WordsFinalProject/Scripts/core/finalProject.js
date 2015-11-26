72ef386472ff68f29c712c8131571e845a2c6ab8
liliya0artyuh <liliya0artyukh@gmail.com>
2015-11-25 22:36:14 -0500

    separated references

--- WordsFinalProject/Scripts/core/finalProject.js
+++ WordsFinalProject/Scripts/core/finalProject.js
@@ -4,33 +4,7 @@
 //    <!--Creation Date: 06 - Nov - 2015 -- >
 //    <!--Last Modified Date: 12 - Nov - 2015 -- >
 //    <!--Last Modified by: Liliya Artyukh -- >
-/// <reference path="../typings/jquery/jquery.d.ts" />
-/// <reference path="../typings/stats/stats.d.ts" />
-/// <reference path="../typings/createjs-lib/createjs-lib.d.ts" />
-/// <reference path="../typings/easeljs/easeljs.d.ts" />
-/// <reference path="../typings/tweenjs/tweenjs.d.ts" />
-/// <reference path="../typings/webaudioapi/waa.d.ts" />
-/// <reference path="../typings/soundjs/soundjs.d.ts" />
-/// <reference path="../typings/preloadjs/preloadjs.d.ts" />
-/// <reference path="../utility/utility.ts" />
-/// <reference path="../config/wordsets.ts" />
-/// <reference path="../config/config.ts" />
-/// <reference path="../managers/asset.ts" />
-/// <reference path="../objects/background.ts" />
-/// <reference path="../objects/word.ts" />
-/// <reference path="../objects/truck.ts" />
-/// <reference path="../objects/label.ts" />
-/// <reference path="../objects/button.ts" />
-/// <reference path="../objects/scene.ts" />
-/// <reference path="../states/instructions.ts" />
-/// <reference path="../states/start.ts" />
-/// <reference path="../states/end.ts" />
-/// <reference path="../states/score.ts" />
-/// <reference path="../states/level1.ts" />
-/// <reference path="../states/level2.ts" />
-/// <reference path="../states/level3.ts" />
-/// <reference path="../objects/scoreboard.ts" />
-/// <reference path="../managers/collision.ts" />
+/// <reference path="_reference.ts" />
 // Global Game Framework Variables
 var canvas;
 var stage;
